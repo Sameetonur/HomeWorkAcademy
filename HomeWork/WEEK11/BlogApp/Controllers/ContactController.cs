9d78caa349e5e79d8098e6fe5674b5d9a544e262
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-10-24 20:34:04 +0300

    24.10.2024 Ödev Bitiş

--- HomeWork/WEEK11/BlogApp/Controllers/ContactController.cs
+++ HomeWork/WEEK11/BlogApp/Controllers/ContactController.cs
@@ -1,13 +1,36 @@
+using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: ContactController
+        List<ContactModel> contactModels;
+
+        public ContactController()
+        {
+            contactModels = [
+                new ContactModel {
+                     AdSoyad="Samet Önür" , Email="Sametoonur@gmail.com", Konu="C# MVC yapısı oluşturmak istiyorum", Mesaj="Merhabalar bana ulaşmak için bu alanı kullanabilirsiniz."
+                }
+            ];
+
+        }
         public ActionResult Index()
         {
+            ViewBag.ContactList = contactModels;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit()
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Index");
+
         }
 
     }
--- HomeWork/WEEK11/BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ HomeWork/WEEK11/BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb79cf4c6c34262b752e55ed2111441902c8609")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db20b1987c6dd13c1eb6ab55965c7ae0943bfcb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
