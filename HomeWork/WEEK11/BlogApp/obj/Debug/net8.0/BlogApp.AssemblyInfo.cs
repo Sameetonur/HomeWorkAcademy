e26b150a1ca5e346a969bbe548eb21b8b8654943
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-10-28 23:17:18 +0300

    28.10.2024

--- HomeWork/WEEK11/BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ HomeWork/WEEK11/BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd211c82efbd7176debad0a5854f29eaf1b4e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c66f8ea28c4ebd3dad053bce63a82a57e21ce59d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
